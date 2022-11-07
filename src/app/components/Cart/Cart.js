2296c0ee3c81d09d6b2be05dac7c7408c40990a0
Julia <julia.dzianisenka@gmail.com>
2022-11-07 16:07:43 +0100

    implement item removing from the cart

--- src/app/components/Cart/Cart.js
+++ src/app/components/Cart/Cart.js
@@ -1,17 +1,27 @@
 import { useSelector, useDispatch } from "react-redux"
+import { removeItem } from "../../features/CartSlice";
 
 export function Cart() {
 
     const cart = useSelector(state => state.cart);
     const dispatch = useDispatch();
 
+    const onRemoveHandler = (item) => {
+        dispatch(removeItem(item));
+    }
+
     return (
         <div>
-            <h1>{cart.length === 0 ? 'Your cart is empty' : ''}</h1>
-            <div>
+            <h1>{cart.length === 0 ? 'Your cart is empty' : 'Your Order'}</h1>
+            <div className="cartList">
                 {
                     cart.map(item => {
-                        return <h1>{item.name}</h1>
+                        return (
+                            <div>
+                                <h1>{item.name}</h1>
+                                <button onClick={()=>{onRemoveHandler(item)}}>-</button>
+                            </div>
+                        )
                     } )
                 }
             </div>
